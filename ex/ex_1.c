06fa9d5d812708bb31d057f5f4c2216ef5adcd25
John Crow <johna.crow@corteva.com>
2019-12-09 10:46:01 -0600

    Exercising pqueue_push() and pqueue_pop().

--- ex/ex_1.c
+++ ex/ex_1.c
@@ -5,5 +5,28 @@
 int
 main(int argc, char *argv[])
 {
+   double      priority;
+   char        x[100];
+
+   struct pqueue *z = pqueue_new();
+
+   pqueue_push(z, 1.0, "pig");
+   pqueue_push(z, 2.0, "cat");
+   pqueue_push(z, 100.0, "dog");
+   pqueue_push(z, 30.0, "cow");
+
+   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, &x);
+
+   /* Testing an empty pqueue */
+   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, &x);
+
+   pqueue_free(z);
+
    return 0;
 }
