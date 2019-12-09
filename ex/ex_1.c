5793c331c3f8e41fe4deb0335df6d8ef45648bd5
John Crow <johna.crow@corteva.com>
2019-12-09 12:40:53 -0600

    Fixed example, more calls.

--- ex/ex_1.c
+++ ex/ex_1.c
@@ -3,11 +3,10 @@
 #include "pqueue.h"
 
 int
-main(int argc, char *argv[])
+main(void)
 {
    double      priority;
-   char        x[100];
-
+   char       *x;
    struct pqueue *z = pqueue_new();
 
    pqueue_push(z, 1.0, "pig");
@@ -15,16 +14,39 @@ main(int argc, char *argv[])
    pqueue_push(z, 100.0, "dog");
    pqueue_push(z, 30.0, "cow");
 
-   pqueue_pop(z, &priority, &x);
-   pqueue_pop(z, &priority, &x);
-   pqueue_pop(z, &priority, &x);
-   pqueue_pop(z, &priority, &x);
+   pqueue_peek(z, &priority, (void **) &x);
+   printf("Peeking I see ... %s is at the head with priority %10.2f\n", x, priority);
+
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Popping I see ... %s is at the head with priority %10.2f\n", x, priority);
+   printf("Empty? %d should be 0\n", pqueue_is_empty(z));
+
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Popping I see ... %s is at the head with priority %10.2f\n", x, priority);
+   printf("Empty? %d should be 0\n", pqueue_is_empty(z));
+
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Popping I see ... %s is at the head with priority %10.2f\n", x, priority);
+   printf("Empty? %d should be 0\n", pqueue_is_empty(z));
+
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Popping I see ... %s is at the head with priority %10.2f\n", x, priority);
+   printf("Empty? %d should be 1\n", pqueue_is_empty(z));
+
+   pqueue_push(z, -100.0, "horse");
+   pqueue_peek(z, &priority, (void **) &x);
+   printf("Peeking I see ... %s is at the head with priority %10.2f\n", x, priority);
+   printf("Empty? %d should be 0\n", pqueue_is_empty(z));
 
    /* Testing an empty pqueue */
-   pqueue_pop(z, &priority, &x);
-   pqueue_pop(z, &priority, &x);
-   pqueue_pop(z, &priority, &x);
-   pqueue_pop(z, &priority, &x);
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Empty? %d should be 1\n", pqueue_is_empty(z));
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Empty? %d should be 1\n", pqueue_is_empty(z));
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Empty? %d should be 1\n", pqueue_is_empty(z));
+   pqueue_pop(z, &priority, (void **) &x);
+   printf("Empty? %d should be 1\n", pqueue_is_empty(z));
 
    pqueue_free(z);
 
