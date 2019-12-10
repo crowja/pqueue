491c29b9e635e6e8f0faa4008eee988300c91678
jac <crowja@gmail.com>
2019-12-09 19:33:06 -0600

    pqueue_peek() added to example.

--- ex/ex_2.c
+++ ex/ex_2.c
@@ -6,7 +6,7 @@
 int
 main(void)
 {
-   unsigned    i, n = 10000;
+   unsigned    i, n = 100000;
    int         rc;
    double      u;
    double      priority;
@@ -15,12 +15,14 @@ main(void)
 
    for (i = 0; i < n; i++) {
       u = rand() / (double) RAND_MAX;
-      if (u < 0.5) {                             /* push */
+      if (u < 0.3) {                             /* push */
          priority = rand();
          x = malloc(20 * sizeof(char));
          strcpy(x, "abcdefg");
          pqueue_push(z, priority, (void *) x);
       }
+      else if (u < 0.6)
+         pqueue_peek(z, &priority, (void **) &x);
       else {                                     /* pop */
          rc = pqueue_pop(z, &priority, (void **) &x);
          if (rc)
@@ -30,7 +32,9 @@ main(void)
 
    while (rc = pqueue_pop(z, &priority, (void **) &x)) {
       if (rc) {
+#if 0
          printf("%10.2f -- %s\n", priority, x);
+#endif
          free(x);
       }
    }
